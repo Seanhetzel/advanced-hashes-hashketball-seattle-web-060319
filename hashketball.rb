a6988d03d122b092ce8232ab818d74ae7f37d998
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-19 23:51:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -122,10 +122,12 @@ end
 
 def num_points_scored(player_name)
   game_hash.each do |location, information|
-    binding.pry
-    if information[:players].include?(player_name)
+    information.each do |attribute, attribute_value|
       binding.pry
-      information[:players][player_name][:points]
+      if information[:players].include?(player_name)
+        binding.pry
+        information[:players][player_name][:points]
+      end
     end
   end
 end
