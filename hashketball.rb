f5114a8797a81eb0f42b7c107717311a011238a8
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-20 01:04:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -175,7 +175,7 @@ def player_stats(player_name)
   game_hash.each do |home_or_away, team_info|
     if team_info[:players].include?(player_name)
       
-      player_stats_hash[player_name] = game_hash[home_or_away][:players][player_name]
+      player_stats_hash[nil] = game_hash[home_or_away][:players][player_name]
     end
   end
   player_stats_hash
