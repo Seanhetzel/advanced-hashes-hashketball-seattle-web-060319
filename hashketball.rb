3d60e7a1a238ded777c1601f89207fcffd262cad
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-20 02:01:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -214,7 +214,7 @@ def winning_team
   game_hash.each do |home_or_away, team_info|
     game_hash[home_or_away][:players].each do |player_name, player_stats|
       team_points += player_stats[:points]
-      
+    winning_team_hash[team_points] = game_hash[home_or_away][:team_name]    
   #      team_w_most_points = team_info[:team_name]
       end
     end
