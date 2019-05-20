0e488aac85a2fe04ba6cf7750c361a2555c14a36
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-20 00:28:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,7 +143,7 @@ end
 def team_colors(team)
   team_colors_array = []
   game_hash.each do |home_or_away, information|
-    team_colors_array << game_hash[home_or_away][:colors]
+    team_colors_array.push(game_hash[home_or_away][:colors])
   end
   team_colors_array
 end
