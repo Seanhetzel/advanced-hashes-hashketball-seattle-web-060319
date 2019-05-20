de063df01f09cbdd5669076863529a0d95380f5a
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-20 01:43:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,6 +199,7 @@ def most_points_scored
   game_hash.each do |home_or_away, team_info|
     game_hash[home_or_away][:players].each do |player_name, player_stats|
       if player_stats[:points] > most_player_points
+        most_player_points = player_stats[:points]
         player_w_most_points = player_name
       end
     end
